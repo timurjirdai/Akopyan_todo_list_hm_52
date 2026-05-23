f49e9c98df45c810b74a8b3ab582f14d26fe2663
artemkuprianov <pokusaevaelina@gmail.com>
2026-05-23 23:44:00 +0600

    Connect tasks urls to project

--- project/tasks/views.py
+++ project/tasks/views.py
@@ -5,6 +5,6 @@ from .models import Task
 def task_list(request):
     tasks = Task.objects.all()
 
-    return render(request, 'tasks/task_list.html', {
+    return render(request, 'tasks/templates/task_list.html', {
         'tasks': tasks
     })
