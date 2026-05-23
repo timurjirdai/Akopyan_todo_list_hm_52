123566ff36127073ac849c005dbb361a3828703c
artemkuprianov <pokusaevaelina@gmail.com>
2026-05-23 23:40:42 +0600

    Create list view

--- project/tasks/models.py
+++ project/tasks/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Task(models.Model):
-
     STATUS_CHOICES = [
         ('new', 'Новая'),
         ('in_progress', 'В процессе'),
@@ -9,17 +8,8 @@ class Task(models.Model):
     ]
 
     description = models.TextField()
-
-    status = models.CharField(
-        max_length=20,
-        choices=STATUS_CHOICES,
-        default='new'
-    )
-
-    due_date = models.DateField(
-        null=True,
-        blank=True
-    )
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='new')
+    date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.description
