79bab587bc799e761cb2921e949fafe1b28236a7
artemkuprianov <pokusaevaelina@gmail.com>
2026-05-23 23:59:50 +0600

    Add url for create form

--- project/tasks/urls.py
+++ project/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import task_list
+from .views import task_list, create_task
 
 urlpatterns = [
     path('', task_list, name='task_list'),
+    path('create/', create_task, name='create_task')
 ]
