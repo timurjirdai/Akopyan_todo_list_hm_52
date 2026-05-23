5ee4458c29bf253b1af9bb0b30f158c8855d0a06
artemkuprianov <pokusaevaelina@gmail.com>
2026-05-23 16:40:58 +0600

    Add Task model and migrations

--- project/tasks/models.py
+++ project/tasks/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+
+    STATUS_CHOICES = [
+        ('new', 'Новая'),
+        ('in_progress', 'В процессе'),
+        ('done', 'Сделано'),
+    ]
+
+    description = models.TextField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='new'
+    )
+
+    due_date = models.DateField(
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.description
